214b6bacbaf3b11c224c19763aa37a12b5eaace1
jasmeen14 <jasmeen6199@gmail.com>
2022-02-22 20:44:38 +0100

    updated javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+//catche DOM
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");//dom variable
+const computerScore_span = document.getElementById("computer-score");// dom variable
+const scoreBoard_span = document.querySelector(".score-board");
