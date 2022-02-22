ad5a067a288186ef25da9349c9c583d12c0392c3
jasmeen14 <jasmeen6199@gmail.com>
2022-02-22 21:01:24 +0100

    changes in js file

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 //catche DOM
 const userScore = 0;
 const computerScore = 0;
-const userScore_span = document.getElementById("user-score");//dom variable
+const userScore_span = document.getElementById("user-score");// dom variable
 const computerScore_span = document.getElementById("computer-score");// dom variable
-const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const scoreBoard_div = document.querySelector(".score-board");// dom variable
+const result_div = document.querySelector(".result");// dom variable
+const rock_div = document.getElementById("r"); // dom variable
+const paper_div = document.getElementById("p");// dom variable
+const scissors_div = document.getElementById("s");// dom variable
+
+functiongame(){
+    Gamepad()
+}
+function main(){
+    rock_div.addEventListener("click",fuction(){
+        game("r");
+      });
+      paper_div.addEventListener("click",fuction(){
+        game("p");
+      });
+      scissors_div.addEventListener("click",fuction(){
+        game("s");
+      });
+}
+main();
+
+
