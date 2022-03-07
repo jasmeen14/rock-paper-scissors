4ee206542a98645ba193f4292060f44181b510f7
jasmeen14 <jasmeen6199@gmail.com>
2022-03-07 21:00:10 +0100

    changes in js file

--- main.js
+++ main.js
@@ -1,32 +1,9 @@
-//catche DOM
-const userScore = 0;
-const computerScore = 0;
-const userScore_span = document.getElementById("user-score");// dom variable
-const computerScore_span = document.getElementById("computer-score");// dom variable
-const scoreBoard_div = document.querySelector(".score-board");// dom variable
-const result_div = document.querySelector(".result");// dom variable
-const rock_div = document.getElementById("r"); // dom variable
-const paper_div = document.getElementById("p");// dom variable
-const scissors_div = document.getElementById("s");// dom variable
-
-function game(userChoice){
-    console.log("choice"+ userChoice);
+var choices = ["paper", "rock","scissors"];
+var i = Math(Math.random() * 3);
+var ComputerChoice = 0;
+var userPoints = 0;
+var ComputerPoints =0;
+function score(){
+   var score_div = document.getElementById("")
 }
-
-//function main(){
-    rock_div.addEventListener("click", fuction())
-        console.log('rock');//
-       // game("r");
-     // }
-      paper_div.addEventListener("click",fuction())
-      {
-        console.log('paper');//game('p');
-      }
-      scissors_div.addEventListener("click",fuction())
-      {
-        console.log('scissors'); //game('s');
-      }
-    
-main();
-
 
